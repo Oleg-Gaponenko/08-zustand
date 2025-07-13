d857f1f0357cca0333959b938bf08801a82e7368
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-07-13 13:33:05 +0300

    add improvements to the app

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,12 +1,20 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
+import getQueryClient from '@/lib/getQueryClient';
+import NoteDetails from './NoteDetails.client';
 import { Metadata } from 'next';
+
+interface NoteDetailsProps {
+  params: Promise<{ id: string }>;
+}
 
 export async function generateMetadata({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }): Promise<Metadata> {
-  const noteId = Number(params.id);
+  const { id } = await params;
+  const noteId = Number(id);
 
   try {
     const note = await fetchNoteById(noteId);
@@ -21,10 +29,30 @@ export async function generateMetadata({
         images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
       },
     };
-  } catch (error) {
+  } catch {
     return {
       title: 'Note not found | NoteHub',
       description: 'This note does not exist or has been deleted.',
     };
   }
+}
+
+export default async function NoteDetailsPage({ params }: NoteDetailsProps) {
+  const { id } = await params;
+  const noteId = Number(id);
+
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', noteId],
+    queryFn: () => fetchNoteById(noteId),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <NoteDetails />
+    </HydrationBoundary>
+  );
 }
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,14 +1,20 @@
+import { NoteTag } from '@/types/note';
+import NotesClient from './Notes.client';
+import { fetchNotes } from '@/lib/api';
 import { Metadata } from 'next';
+
+interface NotesPageProps {
+  params: Promise<{ slug?: string[] }>;
+}
 
 export async function generateMetadata({
   params,
 }: {
-  params: { slug?: string[] };
+  params: Promise<{ slug?: string[] }>;
 }): Promise<Metadata> {
-  const tag = params.slug?.[0];
-  const initialTag = tag
-    ? tag?.charAt(0).toUpperCase() + tag?.slice(1).toLowerCase()
-    : '';
+  const { slug } = await params;
+  const tag = slug?.[0].toLowerCase();
+  const initialTag = tag ? tag?.charAt(0).toUpperCase() + tag?.slice(1) : '';
 
   const metaTitle = tag
     ? `Notes tagged with "${initialTag}" | NoteHub`
@@ -32,4 +38,16 @@ export async function generateMetadata({
       images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
     },
   };
+}
+
+export default async function NotesPage({ params }: NotesPageProps) {
+  const { slug } = await params;
+  const initialTag = slug?.[0];
+  const tag =
+    initialTag && initialTag.toLowerCase() !== 'all'
+      ? (initialTag as NoteTag)
+      : undefined;
+  const notesData = await fetchNotes({ tag });
+
+  return <NotesClient initialData={notesData} tag={tag} />;
 }
